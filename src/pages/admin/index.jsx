1aa0ae66d1d0e5d4516d94f3083a700795f13b46
vikas2705 <vikas.vikasranjan@gmail.com>
2022-11-04 21:28:50 +0530

    chnages to code on 4th Nov-part1

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -5,16 +5,23 @@ import Sidebar from "../../common/components/SideBar";
 import StatusCards from "../../common/components/StatusCards";
 import MaterialTable from "@material-table/core";
 import { ExportCsv, ExportPdf } from "@material-table/exporters";
+import { Button, Modal } from "react-bootstrap";
 
 import "./admin.css";
 
 const Admin = () => {
     const [ticketsList, setTicketsList] = useState([]);
     const [usersList, setUsersList] = useState([]);
+    const [userModalVisible, setUserModalVisible] = useState(false);
+    const [selectedUserDetails, setSelectedUserDetails] = useState({});
     const userName = localStorage.getItem("name") || "";
 
+    const hideUserModal = () => {
+        setUserModalVisible(false);
+    };
+
     useEffect(() => {
-        // getUsers();
+        getUsers();
         getTickets();
     }, []);
 
@@ -62,7 +69,7 @@ const Admin = () => {
                         <Sidebar />
                     </div>
 
-                    <div className='col-10 admin-main bg-gray-200'>
+                    <div className='col-10 admin-main bg-gray-200 vh-100 overflow-auto'>
                         <div>
                             <h2 className='text-primary'>
                                 Welcome, {userName}
@@ -74,83 +81,243 @@ const Admin = () => {
                         </div>
 
                         <StatusCards />
-                        <MaterialTable
-                            data={ticketsList}
-                            columns={[
-                                {
-                                    title: "ID",
-                                    field: "id",
-                                },
-                                {
-                                    title: "TITLE",
-                                    field: "title",
-                                },
-                                {
-                                    title: "DESCRIPTION",
-                                    field: "description",
-                                    filtering: false,
-                                },
-                                {
-                                    title: "REPORTER",
-                                    field: "reporter",
-                                },
-                                {
-                                    title: "PRIORITY",
-                                    field: "ticketPriority",
-                                },
-                                {
-                                    title: "ASSIGNEE",
-                                    field: "assignee",
-                                },
-                                {
-                                    title: "STATUS",
-                                    field: "status",
-                                    lookup: {
-                                        OPEN: "OPEN",
-                                        IN_PROGRESS: "IN_PROGRESS",
-                                        BLOCKED: "BLOCKED",
-                                        CLOSED: "CLOSED",
-                                    },
-                                },
-                            ]}
-                            options={{
-                                filtering: true,
-                                sorting: true,
-                                headerStyle: {
-                                    backgroundColor: "darkblue",
-                                    color: "#FFF",
-                                },
-                                rowStyle: {
-                                    backgroundColor: "#EEE",
-                                },
-                                exportMenu: [
+                        <div className='my-5 px-4'>
+                            <MaterialTable
+                                data={ticketsList}
+                                columns={[
                                     {
-                                        label: "Export PDF",
-                                        exportFunc: (cols, datas) =>
-                                            ExportPdf(
-                                                cols,
-                                                datas,
-                                                "TicketRecords"
-                                            ),
+                                        title: "ID",
+                                        field: "id",
                                     },
                                     {
-                                        label: "Export CSV",
-                                        exportFunc: (cols, datas) =>
-                                            ExportCsv(
-                                                cols,
-                                                datas,
-                                                "TicketRecords"
-                                            ),
+                                        title: "TITLE",
+                                        field: "title",
                                     },
-                                ],
-                            }}
-                            title={"hello"}
-                            onRowClick={() => {
-                                console.log("hello");
-                            }}
-                        />
+                                    {
+                                        title: "DESCRIPTION",
+                                        field: "description",
+                                        filtering: false,
+                                    },
+                                    {
+                                        title: "REPORTER",
+                                        field: "reporter",
+                                    },
+                                    {
+                                        title: "PRIORITY",
+                                        field: "ticketPriority",
+                                    },
+                                    {
+                                        title: "ASSIGNEE",
+                                        field: "assignee",
+                                    },
+                                    {
+                                        title: "STATUS",
+                                        field: "status",
+                                        lookup: {
+                                            OPEN: "OPEN",
+                                            IN_PROGRESS: "IN_PROGRESS",
+                                            BLOCKED: "BLOCKED",
+                                            CLOSED: "CLOSED",
+                                        },
+                                    },
+                                ]}
+                                options={{
+                                    filtering: true,
+                                    sorting: true,
+                                    search: true,
+                                    headerStyle: {
+                                        backgroundColor: "darkblue",
+                                        color: "#FFF",
+                                    },
+                                    rowStyle: {
+                                        backgroundColor: "#EEE",
+                                    },
+                                    exportMenu: [
+                                        {
+                                            label: "Export PDF",
+                                            exportFunc: (cols, datas) =>
+                                                ExportPdf(
+                                                    cols,
+                                                    datas,
+                                                    "Ticket Records CRM"
+                                                ),
+                                        },
+                                        {
+                                            label: "Export CSV",
+                                            exportFunc: (cols, datas) =>
+                                                ExportCsv(
+                                                    cols,
+                                                    datas,
+                                                    "Ticket Records CRM"
+                                                ),
+                                        },
+                                    ],
+                                }}
+                                title={"Ticket records"}
+                                onRowClick={(event, rowData) => {
+                                    console.log(event);
+                                    console.log(rowData);
+                                }}
+                            />
+                        </div>
+                        <div className='my-5 px-4'>
+                            <MaterialTable
+                                data={usersList}
+                                columns={[
+                                    {
+                                        title: "Email",
+                                        field: "email",
+                                    },
+                                    {
+                                        title: "NAME",
+                                        field: "name",
+                                    },
+                                    {
+                                        title: "User ID",
+                                        field: "userId",
+                                    },
+                                    {
+                                        title: "STATUS",
+                                        field: "userStatus",
+                                        lookup: {
+                                            PENDING: "PENDING",
+                                            APPROVED: "APPROVED",
+                                            REJECTED: "REJECTED",
+                                        },
+                                    },
+                                    {
+                                        title: "User Type",
+                                        field: "userTypes",
+                                        lookup: {
+                                            ADMIN: "ADMIN",
+                                            CUSTOMER: "CUSTOMER",
+                                            ENGINEER: "ENGINEER",
+                                        },
+                                    },
+                                ]}
+                                options={{
+                                    filtering: true,
+                                    sorting: true,
+                                    search: true,
+                                    headerStyle: {
+                                        backgroundColor: "darkblue",
+                                        color: "#FFF",
+                                    },
+                                    rowStyle: {
+                                        backgroundColor: "#EEE",
+                                    },
+                                    exportMenu: [
+                                        {
+                                            label: "Export PDF",
+                                            exportFunc: (cols, datas) =>
+                                                ExportPdf(
+                                                    cols,
+                                                    datas,
+                                                    "User Records CRM"
+                                                ),
+                                        },
+                                        {
+                                            label: "Export CSV",
+                                            exportFunc: (cols, datas) =>
+                                                ExportCsv(
+                                                    cols,
+                                                    datas,
+                                                    "User Records CRM"
+                                                ),
+                                        },
+                                    ],
+                                }}
+                                title={"User records"}
+                                onRowClick={(event, rowData) => {
+                                    setSelectedUserDetails({ ...rowData });
+                                    setUserModalVisible(true);
+                                }}
+                            />
+                        </div>
                     </div>
                 </div>
+
+                <Modal
+                    show={userModalVisible}
+                    keyboard='false'
+                    onHide={hideUserModal}
+                    backdrop='static'
+                    centered
+                >
+                    <Modal.Header>
+                        <Modal.Title>Update User Record</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <p className='m-2'>
+                            UserId: {selectedUserDetails.userId}
+                        </p>
+
+                        <div className='form-container my-4 mx-2'>
+                            <label for='name' className='d-flex'>
+                                <span>Name:</span>
+                                <input
+                                    type='text'
+                                    id='name'
+                                    className='form-control  mx-2'
+                                    value={selectedUserDetails.name}
+                                ></input>
+                            </label>
+                        </div>
+
+                        <div className='form-container my-4 mx-2'>
+                            <label for='email' className='d-flex'>
+                                <span> Email:</span>
+                                <input
+                                    type='email'
+                                    id='email'
+                                    className='form-control  mx-2'
+                                    value={selectedUserDetails.email}
+                                ></input>
+                            </label>
+                        </div>
+
+                        <div className='form-container my-4 mx-2'>
+                            <label for='userType' className='d-flex'>
+                                <span> User Type:</span>
+                                <input
+                                    type='text'
+                                    id='userType'
+                                    className='form-control  mx-2'
+                                    disabled
+                                    value={selectedUserDetails.userTypes}
+                                ></input>
+                            </label>
+                        </div>
+
+                        <div className='form-container my-4 mx-2'>
+                            <label for='userStatus' className='d-flex'>
+                                <span> User Status:</span>
+                                <select
+                                    className='form-select mx-2'
+                                    value={selectedUserDetails.userStatus}
+                                >
+                                    <option value='PENDING'>PENDING</option>
+                                    <option value='APPROVED'>APPROVED</option>
+                                    <option value='REJECTED'>REJECTED</option>
+                                </select>
+                            </label>
+                        </div>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button
+                            type='submit'
+                            className='btn btn-secondary'
+                            onClick={hideUserModal}
+                        >
+                            Cancel
+                        </Button>
+                        <Button type='submit' className='btn btn-primary'>
+                            Save
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         </div>
     );
