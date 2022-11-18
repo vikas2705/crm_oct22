ac8b557cd21af150302811d0465f984bcfec3ed2
vikas2705 <vikas.vikasranjan@gmail.com>
2022-11-18 21:20:20 +0530

    Downgrading react and upgrading material and core-ui packages

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ErrorBoundary } from "./common/components/ErrorBoundary";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <ErrorBoundary>
                 <App />
             </ErrorBoundary>
         </BrowserRouter>
-    </React.StrictMode>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
